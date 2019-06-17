a954381e39f16c8eb9b5426bec94402482bec790
erickogi <erickogi14@gmail.com>
2019-06-17 08:01:06 +0300

    Initial Commit

--- app/src/main/java/com/kogicodes/pawametinderforcats/adapters/CatAdapter.kt
+++ app/src/main/java/com/kogicodes/pawametinderforcats/adapters/CatAdapter.kt
@@ -1,6 +1,18 @@
 
-package com.kogicodes.pawametinderforcats.adapters
+/*
+ * Copyright 2019 Eric Kogi. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.kogicodes.pawametinderforcats.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
@@ -11,7 +23,6 @@ import com.kogicodes.pawametinderforcats.R
 import com.kogicodes.pawametinderforcats.model.Cat
 import com.kogicodes.pawametinderforcats.ui.uiUtils.OnRecyclerViewItemClick
 import com.kogicodes.pawametinderforcats.ui.uiUtils.ViewUtils
-import kotlinx.android.synthetic.main.cat_card.*
 
 class CatAdapter(
     private var context: Context,
